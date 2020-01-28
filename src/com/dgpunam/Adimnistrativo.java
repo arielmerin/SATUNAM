c926a1bcec80ee92ba019a8dfe16e16ba60f23a6
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-28 16:49:42 -0600

    Este tiene completo los constructores de las clases principales

--- src/com/dgpunam/Adimnistrativo.java
+++ src/com/dgpunam/Adimnistrativo.java
@@ -3,11 +3,16 @@ package com.dgpunam;
 public class Adimnistrativo extends Trabajador {
 
     private String lugarTrabajo;
-    private String Puesto;
-    private int horario;
+    private String puesto;
+    private Horario horario;
 
 
-    public Adimnistrativo(String nombre, String apellidoMaterno, String apellidoPaterno, String curp, String direccion, String correoElectronico, int matricula, int sueldo, int antiguedad, String lugarTrabajo, String puesto, int horario) {
+    public Adimnistrativo(String nombre, String apellidoMaterno, String apellidoPaterno, String curp, String direccion,
+                          String correoElectronico, int matricula, int sueldo, int antiguedad, String lugarTrabajo,
+                          String puesto, Horario horario) {
         super(nombre, apellidoMaterno, apellidoPaterno, curp, direccion, correoElectronico, matricula, sueldo, antiguedad);
+        this.horario = horario;
+        this.lugarTrabajo = lugarTrabajo;
+        this.puesto = puesto;
     }
 }
